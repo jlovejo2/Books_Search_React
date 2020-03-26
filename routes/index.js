5d7cb0cf7706ac7079f53da9ef63474316a47ffc
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-25 20:05:20 -0500

    fixed proxy error, added axios call to googlebooksApi and send data back to client side

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,33 @@ const express = require('express');
 
 //calls the express.router method
 const router = express.Router();
-
+const axios = require('axios');
 // const booksController = require("../../controllers/booksController");
 
 const apiKey = process.env.MY_GOOGLE_BOOKS_API_KEY
 
 router.get('/api/googleBooks/:selectValue/:inputValue', function(req,res) {
 
-    console.log(req.params.query);
+    let setQuery ='';
 
-    if(selectValue === 'Title') {
-        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + inputValue + "+intitle:" + inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
-    } else if (selectValue === 'Author') {
-        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + inputValue + "+inauthor" + inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
-    } else if (selectValue ==='Subject') {
-        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + inputValue + "+subject" + inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
+    if(req.params.selectValue === 'Title') {
+        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + req.params.inputValue + "+intitle:" + req.params.inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
+    } else if (req.params.electValue === 'Author') {
+        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + req.params.inputValue + "+inauthor" + req.params.inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
+    } else if (req.params.selectValue ==='Subject') {
+        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + req.params.inputValue + "+subject" + req.params.inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
     } else {    
-        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
+        setQuery = "https://www.googleapis.com/books/v1/volumes?q=" + req.params.inputValue + "&printType=books&orderBy=relevance&key=" + apiKey;
     }
+
+    axios.get(setQuery)
+        .then( booksObj => {
+            const receivedData = booksObj.data.items
+            console.log(booksObj.data);
+            res.json(receivedData);
+        })
+        .catch(err => console.log(err))
+
 })
 
 
